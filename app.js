9eb49f4ad166fe541b16eb8cab82daf8168c1e9f
rachelsalazar <ret1992@gmail.com>
2016-10-03 18:20:54 -0700

    counter and numTimesShown are working

--- app.js
+++ app.js
@@ -3,34 +3,34 @@
 var imageContainer = document.getElementById('image-container');
 var allImages = [];
 
-function Product(productName, filePath){
+function Product(productName, filePath, numTimesShown){
   this.productName = productName;
   this.filePath = filePath;
-  this.numTimesShown = 0;
-  this.numTimesCicked = 0;
+  this.numTimesShown = numTimesShown;
+  // this.numTimesClicked = numTimesClicked;
   allImages.push(this);
 };
 
-new Product('bag', 'images/bag.jpg');
-new Product('banana', 'images/banana.jpg');
-new Product('bathroom', 'images/bathroom.jpg');
-new Product('boots', 'images/boots.jpg');
-new Product('breakfast', 'images/breakfast.jpg');
-new Product('bubblegum', 'images/bubblegum.jpg');
-new Product('chair', 'images/chair.jpg');
-new Product('cthulhu', 'images/cthulhu.jpg');
-new Product('dog-duck', 'images/dog-duck.jpg');
-new Product('dragon', 'images/dragon.jpg');
-new Product('pen', 'images/pen.jpg');
-new Product('pet-sweep', 'images/pet-sweep.jpg');
-new Product('scissors', 'images/scissors.jpg');
-new Product('shark', 'images/shark.jpg');
-new Product('sweep', 'images/sweep.png');
-new Product('tauntaun', 'images/tauntaun.jpg');
-new Product('unicorn', 'images/unicorn.jpg');
-new Product('usb', 'images/usb.gif');
-new Product('water-can', 'images/water-can.jpg');
-new Product('wine-glass', 'images/wine-glass.jpg');
+new Product('bag', 'images/bag.jpg', 0);
+new Product('banana', 'images/banana.jpg', 0);
+new Product('bathroom', 'images/bathroom.jpg', 0);
+new Product('boots', 'images/boots.jpg', 0);
+new Product('breakfast', 'images/breakfast.jpg',0);
+new Product('bubblegum', 'images/bubblegum.jpg', 0);
+new Product('chair', 'images/chair.jpg', 0);
+new Product('cthulhu', 'images/cthulhu.jpg', 0);
+new Product('dog-duck', 'images/dog-duck.jpg', 0);
+new Product('dragon', 'images/dragon.jpg', 0);
+new Product('pen', 'images/pen.jpg', 0);
+new Product('pet-sweep', 'images/pet-sweep.jpg', 0);
+new Product('scissors', 'images/scissors.jpg', 0);
+new Product('shark', 'images/shark.jpg', 0);
+new Product('sweep', 'images/sweep.png', 0);
+new Product('tauntaun', 'images/tauntaun.jpg', 0);
+new Product('unicorn', 'images/unicorn.jpg', 0);
+new Product('usb', 'images/usb.gif', 0);
+new Product('water-can', 'images/water-can.jpg', 0);
+new Product('wine-glass', 'images/wine-glass.jpg', 0);
 
 var left = document.getElementById('left');
 var center = document.getElementById('center');
@@ -39,19 +39,38 @@ var right = document.getElementById('right');
 var leftIndex;
 var centerIndex;
 var rightIndex;
+var counter = 0;
 
-var randomIndex = function() {
-  for (var i = 0; i < allImages.length; i++) {
-    leftIndex = Math.floor(Math.random() * allImages.length);
-    centerIndex = Math.floor(Math.random() * allImages.length);
-    rightIndex = Math.floor(Math.random() * allImages.length);
-  }
-};
-randomIndex();
+render();
 
-while ((leftIndex === centerIndex) || (centerIndex === rightIndex) || (leftIndex === rightIndex)) {
+//function rendered on click
+function render() {
+  var randomIndex = function() {
+    for (var i = 0; i < allImages.length; i++) {
+      leftIndex = Math.floor(Math.random() * allImages.length);
+      centerIndex = Math.floor(Math.random() * allImages.length);
+      rightIndex = Math.floor(Math.random() * allImages.length);
+    }
+  };
   randomIndex();
-}
-left.src = allImages[leftIndex].filePath;
-center.src = allImages[centerIndex].filePath;
-right.src = allImages[rightIndex].filePath;
+//making sure there are no duplicate images
+  while ((leftIndex === centerIndex) || (centerIndex === rightIndex) || (leftIndex === rightIndex)) {
+    randomIndex();
+  }
+  left.src = allImages[leftIndex].filePath;
+  allImages[leftIndex].numTimesShown += 1;
+  console.log(left.src + ' has been shown ' + allImages[leftIndex].numTimesShown);
+  center.src = allImages[centerIndex].filePath;
+  right.src = allImages[rightIndex].filePath;
+
+};
+
+var userClicks = function() {
+  onclick = counter++;
+  if (counter > 24) {
+    imageContainer.removeEventListener('click', userClicks);
+  }
+  render();
+};
+
+imageContainer.addEventListener('click', userClicks);
